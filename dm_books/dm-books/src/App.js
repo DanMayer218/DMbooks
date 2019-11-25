89d2611509e294747cfcaebc8023648e5231aa7a
Daniel Mayer <danmayercraigs@gmail.com>
2019-11-24 23:03:12 -0500

    first commit after the create react app boiler plate and some additions including a components folder and a utils folder to the src folder

---
+++ dm_books/dm-books/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Saved from "./components/Pages/Saved";
+import Search from "./components/Pages/Search";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ dm_books/dm-books/src/components/Pages/Saved.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
+
+function Saved() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Saved;
